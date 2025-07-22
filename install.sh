6c31fd004669a5a2a013d003d2d44edecde85fc8
LVMSN <leovannys@Mini4.local>
2025-07-22 22:33:59 +0800

    Refactor install.sh: remove pause function and update script output for clarity

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 # 等待1秒, 避免curl下载脚本的打印与脚本本身的显示冲突, 吃掉了提示用户按回车继续的信息
 sleep 1
 
-echo -e "                     _ ___                   \n ___ ___ __ __ ___ _| |  _|___ __ __   _ ___ \n|-_ |_  |  |  |-_ | _ |   |- _|  |  |_| |_  |\n|___|___|  _  |___|___|_|_|___|  _  |___|___|\n        |_____|               |_____|        "
 red='\e[91m'
 green='\e[92m'
 yellow='\e[93m'
@@ -17,18 +16,23 @@ warn() {
     echo -e "\n$yellow $1 $none\n"
 }
 
+# Remove pause function or modify it to do nothing
 pause() {
-    read -rsp "$(echo -e "按 $green Enter 回车键 $none 继续....或按 $red Ctrl + C $none 取消.")" -d $'\n'
-    echo
+    return 0
 }
 
 # 说明
-echo
-echo -e "$yellow此脚本仅兼容于Debian 10+系统. 如果你的系统不符合,请Ctrl+C退出脚本$none"
-echo -e "可以去 ${cyan}https://github.com/crazypeace/xray-vless-reality${none} 查看脚本整体思路和关键命令, 以便针对你自己的系统做出调整."
-echo -e "有问题加群 ${cyan}https://t.me/+ISuvkzFGZPBhMzE1${none}"
-echo -e "本脚本支持带参数执行, 省略交互过程, 详见GitHub."
-echo "----------------------------------------------------------------"
+
+
+echo "██╗     ██╗██╗   ██╗███████╗███████╗██████╗ ███████╗███████╗";
+echo "██║     ██║██║   ██║██╔════╝██╔════╝██╔══██╗██╔════╝██╔════╝";
+echo "██║     ██║██║   ██║█████╗  █████╗  ██████╔╝█████╗  █████╗  ";
+echo "██║     ██║╚██╗ ██╔╝██╔══╝  ██╔══╝  ██╔══██╗██╔══╝  ██╔══╝  ";
+echo "███████╗██║ ╚████╔╝ ███████╗██║     ██║  ██║███████╗███████╗";
+echo "╚══════╝╚═╝  ╚═══╝  ╚══════╝╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝";
+echo "                                                            ";
+echo -e "${cyan}https://github.com/livingfree2023/xray-vless-reality-livefree${none} "
+echo -e "本脚本支持带参数执行, 不带参数将直接无敌"
 
 # 本机 IP
 InFaces=($(ls /sys/class/net/ | grep -E '^(eth|ens|eno|esp|enp|venet|vif)'))
@@ -104,8 +108,6 @@ if [ $# -ge 1 ]; then
     echo "----------------------------------------------------------------"
 fi
 
-pause
-
 # 准备工作
 apt update
 apt install -y curl sudo jq qrencode net-tools lsof
@@ -157,70 +159,26 @@ echo "----------------------------------------------------------------"
 
 # 网络栈
 if [[ -z $netstack ]]; then
-  echo
-  echo -e "如果你的小鸡是${magenta}双栈(同时有IPv4和IPv6的IP)${none}，请选择你把Xray搭在哪个'网口'上"
-  echo "如果你不懂这段话是什么意思, 请直接回车"
-  read -p "$(echo -e "Input ${cyan}4${none} for IPv4, ${cyan}6${none} for IPv6:") " netstack
-
-  if [[ $netstack == "4" ]]; then
-    ip=${IPv4}
-  elif [[ $netstack == "6" ]]; then
-    ip=${IPv6}
-  else
     if [[ -n "$IPv4" ]]; then
-      ip=${IPv4}
-      netstack=4
+        netstack=4
+        ip=${IPv4}
     elif [[ -n "$IPv6" ]]; then
-      ip=${IPv6}
-      netstack=6
+        netstack=6
+        ip=${IPv6}
     else
-      warn "没有获取到公共IP"
+        warn "没有获取到公共IP"
+        exit 1
     fi
-  fi
 fi
 
 # 端口
 if [[ -z $port ]]; then
-  default_port=443
-  while :; do
-    read -p "$(echo -e "请输入端口 [${magenta}1-65535${none}] Input port (默认Default ${cyan}${default_port}$none):")" port
-    [ -z "$port" ] && port=$default_port
-    case $port in
-    [1-9] | [1-9][0-9] | [1-9][0-9][0-9] | [1-9][0-9][0-9][0-9] | [1-5][0-9][0-9][0-9][0-9] | 6[0-4][0-9][0-9][0-9] | 65[0-4][0-9][0-9] | 655[0-3][0-5])
-      echo
-      echo
-      echo -e "$yellow 端口 (Port) = ${cyan}${port}${none}"
-      echo "----------------------------------------------------------------"
-      echo
-      break
-      ;;
-    *)
-      error
-      ;;
-    esac
-  done
+  port=10443
 fi
 
 # Xray UUID
 if [[ -z $uuid ]]; then
-  while :; do
-    echo -e "请输入 "$yellow"UUID"$none" "
-    read -p "$(echo -e "(默认ID: ${cyan}${default_uuid}$none):")" uuid
-    [ -z "$uuid" ] && uuid=$default_uuid
-    case $(echo -n $uuid | sed -E 's/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}//g') in
-    "")
-        echo
-        echo
-        echo -e "$yellow UUID = $cyan$uuid$none"
-        echo "----------------------------------------------------------------"
-        echo
-        break
-        ;;
-    *)
-        error
-        ;;
-    esac
-  done
+  uuid=$default_uuid
 fi
 
 # x25519公私钥
@@ -233,7 +191,7 @@ if [[ -z $private_key ]]; then
   default_public_key=$(echo ${tmp_key} | awk '{print $6}')
 
   echo -e "请输入 "$yellow"x25519 Private Key"$none" x25519私钥 :"
-  read -p "$(echo -e "(默认私钥 Private Key: ${cyan}${default_private_key}$none):")" private_key
+  #read -p "$(echo -e "(默认私钥 Private Key: ${cyan}${default_private_key}$none):")" private_key
   if [[ -z "$private_key" ]]; then 
     private_key=$default_private_key
     public_key=$default_public_key
@@ -256,7 +214,7 @@ if [[ -z $shortid ]]; then
   default_shortid=$(echo -n ${uuid} | sha1sum | head -c 16)
   while :; do
     echo -e "请输入 "$yellow"ShortID"$none" :"
-    read -p "$(echo -e "(默认ShortID: ${cyan}${default_shortid}$none):")" shortid
+    #read -p "$(echo -e "(默认ShortID: ${cyan}${default_shortid}$none):")" shortid
     [ -z "$shortid" ] && shortid=$default_shortid
     if [[ ${#shortid} -gt 16 ]]; then
       error
@@ -280,7 +238,7 @@ fi
 # 目标网站
 if [[ -z $domain ]]; then
   echo -e "请输入一个 ${magenta}合适的域名${none} Input the domain"
-  read -p "(例如: learn.microsoft.com): " domain
+  #read -p "(例如: learn.microsoft.com): " domain
   [ -z "$domain" ] && domain="learn.microsoft.com"
 
   echo
@@ -457,39 +415,24 @@ if [[ $netstack == "6" ]]; then
   ip=[$ip]
 fi
 vless_reality_url="vless://${uuid}@${ip}:${port}?flow=xtls-rprx-vision&encryption=none&type=tcp&security=reality&sni=${domain}&fp=${fingerprint}&pbk=${public_key}&sid=${shortid}&spx=${spiderx}&#VLESS_R_${ip}"
-echo -e "${cyan}${vless_reality_url}${none}"
-echo
-sleep 3
-echo "以下两个二维码完全一样的内容"
-qrencode -t UTF8 $vless_reality_url
-qrencode -t ANSI $vless_reality_url
-echo
-echo "---------- END -------------"
-echo "以上节点信息保存在 ~/_vless_reality_url_ 中"
 
-# 节点信息保存到文件中
-echo $vless_reality_url > ~/_vless_reality_url_
-echo "以下两个二维码完全一样的内容" >> ~/_vless_reality_url_
-qrencode -t UTF8 $vless_reality_url >> ~/_vless_reality_url_
-qrencode -t ANSI $vless_reality_url >> ~/_vless_reality_url_
 
 # 如果是 IPv6 小鸡，用 WARP 创建 IPv4 出站
 if [[ $netstack == "6" ]]; then
     echo
     echo -e "$yellow这是一个 IPv6 小鸡，用 WARP 创建 IPv4 出站$none"
     echo "Telegram电报是直接访问IPv4地址的, 需要IPv4出站的能力"
-    echo -e "如果WARP安装不顺利, 请在命令行执行${cyan} bash <(curl -L https://ghproxy.crazypeace.workers.dev/https://github.com/crazypeace/warp.sh/raw/main/warp.sh) 4 ${none}"
+    echo -e "如果需要WARP, 请在命令行执行${cyan} bash <(curl -L https://ghproxy.crazypeace.workers.dev/https://github.com/crazypeace/warp.sh/raw/main/warp.sh) 4 ${none}"
     echo "----------------------------------------------------------------"
-    pause
-
+    
     # 安装 WARP IPv4
-    bash <(curl -L git.io/warp.sh) 4
+    #bash <(curl -L git.io/warp.sh) 4
 
     # 重启 Xray
-    echo
-    echo -e "$yellow重启 Xray$none"
-    echo "----------------------------------------------------------------"
-    service xray restart
+    # echo
+    # echo -e "$yellow重启 Xray$none"
+    # echo "----------------------------------------------------------------"
+    # service xray restart
 
 # 如果是 IPv4 小鸡，用 WARP 创建 IPv6 出站
 elif  [[ $netstack == "4" ]]; then
@@ -499,17 +442,26 @@ elif  [[ $netstack == "4" ]]; then
     echo -e "群组: ${cyan} https://t.me/+ISuvkzFGZPBhMzE1 ${none}"
     echo -e "教程: ${cyan} https://zelikk.blogspot.com/2022/03/racknerd-v2ray-cloudflare-warp--ipv6-google-domainstrategy-outboundtag-routing.html ${none}"
     echo -e "视频: ${cyan} https://youtu.be/Yvvm4IlouEk ${none}"
-    echo -e "如果WARP安装不顺利, 请在命令行执行${cyan} bash <(curl -L https://ghproxy.crazypeace.workers.dev/https://github.com/crazypeace/warp.sh/raw/main/warp.sh) 6 ${none}"
+    echo -e "如果需要WARP, 请在命令行执行${cyan} bash <(curl -L https://ghproxy.crazypeace.workers.dev/https://github.com/crazypeace/warp.sh/raw/main/warp.sh) 6 ${none}"
     echo "----------------------------------------------------------------"
-    pause
 
     # 安装 WARP IPv6
-    bash <(curl -L git.io/warp.sh) 6
+    #bash <(curl -L git.io/warp.sh) 6
 
     # 重启 Xray
-    echo
-    echo -e "$yellow重启 Xray$none"
-    echo "----------------------------------------------------------------"
-    service xray restart
+    #echo
+    #echo -e "$yellow重启 Xray$none"
+    #echo "----------------------------------------------------------------"
+    #service xray restart
 
 fi
+
+echo -e "${cyan}${vless_reality_url}${none}"
+echo
+echo "如果需要二维码，复制以下命令"
+echo "qrencode -t UTF8 '$vless_reality_url'"
+# 节点信息保存到文件中
+echo $vless_reality_url > ~/livefree.reality.txt
+echo "以上节点信息保存在 ~/livefree.reality.txt 中"
+echo "卸载命令：bash -c '$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)' @ remove --purge "
+echo "---------- Live Free & Stay Strong -------------"
